894f234284ca1decf699dea733f32a0a113b06ac
Dominic Hein <dominic.hein@students.fhnw.ch>
2019-04-16 14:01:40 +0200

    CORS & Ajax extension, not fully tested with server yet

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,60 @@
 import Header from './Header';
 
 export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            title: ''
+        };
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:8080/votes/question")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        questionText: result.title
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
     render() {
-        return (
-            <div className="container-fluid">
-                <Header title="Hello from JavaScript Client"
-                        status="This is a status"
-                        isError={true}
-                        />
-            </div>
-        )
+        const { error, isLoaded, questionText } = this.state;
+
+        if (error) {
+            return (
+                <div className="container-fluid">
+                    <Header title="No question"
+                            status="This is a status"
+                            isError={true}
+                    />
+                </div>
+            );
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <div className="container-fluid">
+                    <Header title={questionText}
+                            status="This is a status"
+                            isError={true}
+                    />
+                </div>
+            )
+        }
     }
 }
